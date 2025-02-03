a0366d151f09ac9a91e9aac574c95c8ee0d71360
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-02-03 23:53:47 +0530

    Update ArrayConverter.java

--- ArrayConverter.java
+++ ArrayConverter.java
@@ -1,1 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class ArrayConverter {
+    // Converts an array to an ArrayList
+    public void arrayToArrayList() {
+        String[] arr = {"Apple", "Banana", "Cherry"};
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(arr));
+
+        System.out.println("Converted Array to ArrayList: " + list);
+    }
+
+    public void arrayListToArray() {
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("Dog", "Cat", "Elephant"));
+        String[] arr = list.toArray(new String[0]);
+
+        System.out.println("Converted ArrayList to Array: " + Arrays.toString(arr));
+    }
+}
