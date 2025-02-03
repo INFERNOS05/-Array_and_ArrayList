4cde31a6986926970c771861b1ba36d2a1d47178
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-02-03 23:53:08 +0530

    Update SmallestDistanceFinder.java

--- SmallestDistanceFinder.java
+++ SmallestDistanceFinder.java
@@ -1,1 +1,20 @@
+public class SmallestDistanceFinder {
+    public static int findSmallestDistanceIndex(int[] arr) {
+        if (arr.length < 2) {
+            throw new IllegalArgumentException("Array must have at least two elements.");
+        }
+
+        int minIndex = 0;
+        int minDiff = Math.abs(arr[1] - arr[0]);
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            int diff = Math.abs(arr[i + 1] - arr[i]);
+            if (diff < minDiff) {
+                minDiff = diff;
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+}
 
